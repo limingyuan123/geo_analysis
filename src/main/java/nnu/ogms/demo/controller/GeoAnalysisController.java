769ba905785cf0354767839ab7dd422c6b4b5c4d
limingyuan123 <1484156313@qq.com>
2020-11-27 16:59:25 +0800

    首页路径

--- src/main/java/nnu/ogms/demo/controller/GeoAnalysisController.java
+++ src/main/java/nnu/ogms/demo/controller/GeoAnalysisController.java
@@ -2,6 +2,7 @@ package nnu.ogms.demo.controller;
 
 import com.sun.org.apache.bcel.internal.generic.RET;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -10,8 +11,8 @@ import org.springframework.web.servlet.ModelAndView;
  * @Date 2020.11.20 16:15
  */
 @RestController
-public class GeoAnalysis {
-    @RequestMapping("map")
+public class GeoAnalysisController {
+    @RequestMapping(value={"/","/map"},method = RequestMethod.GET)
     public ModelAndView loadMap(){
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("map");
